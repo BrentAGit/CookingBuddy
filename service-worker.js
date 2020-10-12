b1aa3487987800ddf3bc15e257bf5a7070701901
BrentAGit <r0787709@student.thomasmore.be>
2020-10-13 00:04:48 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BrentAGit/CookingBuddy.git/precache-manifest.1560a1eb39da61b77b36ccd3ccbdb90b.js"
+  "/CookingBuddy/precache-manifest.b60c81049ea0b7d78e121122af9e6151.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BrentAGit/CookingBuddy.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CookingBuddy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
