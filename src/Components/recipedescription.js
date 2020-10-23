fb36ff885334412f15c3cd54960141bbd8bd2665
BrentAGit <r0787709@student.thomasmore.be>
2020-10-23 12:07:57 +0200

    Added styled const StyledRecipeName to add margins to desciptions (for now, until it gets moved to a detail page)

--- src/Components/recipedescription.js
+++ src/Components/recipedescription.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from "@emotion/styled";
 
+const StyledRecipeDescription = styled.div`
+margin-left: 10px;
+`;
+
 export default function RecipeDesc(props) {
 return(
-    <div>
+    <StyledRecipeDescription>
         {props.children}
-    </div>
+    </StyledRecipeDescription>
 )
 }
