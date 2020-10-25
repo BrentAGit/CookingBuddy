36e225cde9766eabd0269e19dd20689d5ef783ce
BrentAGit <r0787709@student.thomasmore.be>
2020-10-25 15:17:04 +0100

    Added actual descriptions in data.js

--- src/Data/data.js
+++ src/Data/data.js
@@ -2,45 +2,45 @@ export default [
     {
         name: "Beef",
         id: 1,
-        recipes: [{name: "Steak", description: "Filler1"},
-            {name: "Ribs", description: "Filler2"},
-            {name: "Beef Sandwich", description: "Filler3"},
-            {name: "Hamburger", description: "Filler4"}]
+        recipes: [{name: "Steak", description: "A recipe for a juicy, tender, and perfectly cooked steak."},
+            {name: "Ribs", description: "Perfectly tender ribs with a marinade of your choice."},
+            {name: "Beef Sandwich", description: "A sandwich made out of juicy beef between two layers of soft bread."},
+            {name: "Hamburger", description: "A thick, restaurant styled burger with whatever toppings you like."}]
     },
 
     {
         name: "Chicken",
         id: 2,
-        recipes: [{name: "Fried Wings", description: "Filler5"},
-            {name: "Chicken soup", description: "Filler6"},
-            {name: "Chicken Katsu", description: "Filler7"},
-            {name: "Buffalo wings", description: "Filler8"}]
+        recipes: [{name: "Fried Wings", description: "Crunchy on the outside, tender on the inside, the perfect fried chicken!"},
+            {name: "Chicken soup", description: "A classic recipe for the best chicken soup. Perfect for a cold day."},
+            {name: "Chicken Katsu", description: "Crispy chicken breast with a crunchy breadcrumb coating, served over a bowl of rice."},
+            {name: "Buffalo wings", description: "Delicious buffalo wings coated with sauce of your choice."}]
     },
 
     {
         name: "Egg",
         id: 3,
-        recipes: [{name: "Hard-boiled egg", description: "Filler9"},
-            {name: "Omlette", description: "Filler10"},
-            {name: "Scrambled egg", description: "Filler11"},
-            {name: "Sunny-side-up egg", description: "Filler12"}]
+        recipes: [{name: "Hard-boiled egg", description: "The easiest recipe for hard-boiled eggs, goes well with about anything!"},
+            {name: "Omlette", description: "A classic, quick and easy omlette."},
+            {name: "Scrambled egg", description: "A quick and easy way to prepare a creamy and delicate meal with eggs."},
+            {name: "Sunny-side-up egg", description: "A creamy, soft and delicate, yet very simple way of preparing and egg. "}]
     },
 
     {
         name: "Pasta",
         id: 4,
-        recipes: [{name: "Spaghetti Bolognese", description: "Filler13"},
-            {name: "Lasagna", description: "Filler14"},
-            {name: "Macaroni and cheese", description: "Filler15"},
-            {name: "Tagliatelle", description: "Filler16"}]
+        recipes: [{name: "Spaghetti Bolognese", description: "A hot bowl of spaghetti swimming in a bolognese sauce."},
+            {name: "Lasagna", description: "Thinly rolled pasta with multiple layers of delicious sauce and cheese."},
+            {name: "Macaroni and cheese", description: "Soft macaroni swimming in a pool of hot and melted cheese."},
+            {name: "Tagliatelle", description: "Another italian way of making a perfect bowl of pasta."}]
     },
 
     {
         name: "Vegetarian",
         id: 5,
-        recipes: [{name: "Ceasar salad", description: "Filler17"},
-            {name: "Roasted carrot and beets", description: "Filler18"},
-            {name: "Vegetable soup", description: "Filler19"},
-            {name: "Cauliflower & roast potato's", description: "Filler20"}]
+        recipes: [{name: "Ceasar salad", description: "A healthy, yet delicious meal with all your favorite vegetables."},
+            {name: "Roasted carrot and beets", description: "A dish including a combination of roasted carrots and beets."},
+            {name: "Vegetable soup", description: "A healthy bowl of soup with vegetables of your choice, perfect for a cold winter day."},
+            {name: "Cauliflower & roast potato's", description: "A delicious combination of cauliflower and roast potato's."}]
     },
 ]
