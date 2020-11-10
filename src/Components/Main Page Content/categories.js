48e1b657dfcd7401017f507ae1ecdb4d94d28017
BrentAGit <r0787709@student.thomasmore.be>
2020-11-10 14:55:18 +0100

    Finally Fixed Hrefs

--- src/Components/Main Page Content/categories.js
+++ src/Components/Main Page Content/categories.js
@@ -22,7 +22,7 @@ export default function Category(props) {
     const {recipes, name} = props
 
     return (
-        <StyledCategory>
+        <StyledCategory  id={name}>
             <StyledCatTitle>
                 {name}
             </StyledCatTitle>
