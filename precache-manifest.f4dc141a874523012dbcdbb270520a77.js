9241387312c9c4aeb64d4333f06ffee4dd4452df
BrentAGit <r0787709@student.thomasmore.be>
2020-11-11 01:43:40 +0100

    Updates

---
+++ precache-manifest.f4dc141a874523012dbcdbb270520a77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b4a4f0f9d26daacec73f3c705c3afb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3bf78b56f1994a23a79c",
+    "url": "./static/css/main.c2cf54d0.chunk.css"
+  },
+  {
+    "revision": "2051c5827e997ff436b3",
+    "url": "./static/js/2.73be3672.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "./static/js/2.73be3672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf78b56f1994a23a79c",
+    "url": "./static/js/main.4b7d007e.chunk.js"
+  },
+  {
+    "revision": "8c18fd9244e6f2a300f2",
+    "url": "./static/js/runtime-main.1cb09ab3.js"
+  },
+  {
+    "revision": "10962b42f860de906081b7471b6e3da3",
+    "url": "./static/media/BackgroundImage.10962b42.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CookingBuddy/precache-manifest.ef95aae9fc10d5db426023a8512f8d5b.js"
+  "./precache-manifest.f4dc141a874523012dbcdbb270520a77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CookingBuddy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
